1eb1b365331ee03b6cc893cd559f3d6b2bce09cb
123Nithish123 <nithishkumarvasu2@gmail.com>
2026-07-10 10:15:22 +0530

    Add Module-11: Blog Application with Post Component

---
+++ Module-11-React/blogapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Posts from './Post';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Blog Application</h1>
+      </header>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
