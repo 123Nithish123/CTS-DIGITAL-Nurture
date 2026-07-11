a8426d42c8ee8b18aab0e92c63f8cc4561df5edd
123Nithish123 <nithishkumarvasu2@gmail.com>
2026-07-11 10:45:33 +0530

    Add Module-11: Cohort Dashboard with Snapshot Tests

---
+++ Module-11-React/cohortdashboard/src/CohortDetails.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './CohortDetails.module.css';
+
+function CohortDetails({ cohort }) {
+  const statusClass = cohort.status === 'ongoing' ? styles.ongoing : styles.completed;
+
+  return (
+    <div className={styles.box}>
+      <h3 className={statusClass}>{cohort.name}</h3>
+      <dl>
+        <dt>Status</dt>
+        <dd>{cohort.status}</dd>
+        <dt>Start Date</dt>
+        <dd>{cohort.startDate}</dd>
+        <dt>End Date</dt>
+        <dd>{cohort.endDate}</dd>
+        <dt>Participants</dt>
+        <dd>{cohort.participants}</dd>
+        <dt>Trainer</dt>
+        <dd>{cohort.trainer}</dd>
+      </dl>
+    </div>
+  );
+}
+
+export default CohortDetails;
