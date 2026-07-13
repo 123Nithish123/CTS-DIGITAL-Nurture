b7981ff3e39deae4b571641abb4020d6ef50a433
123Nithish123 <nithishkumarvasu2@gmail.com>
2026-07-13 14:55:38 +0530

    Add Module-11: Fetch User App with API Integration

---
+++ Module-11-React/fetchuserapp/src/App.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import Getuser from './Getuser';
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Fetch User App</h1>
+      <Getuser />
+    </div>
+  );
+}
+
+export default App;
---
+++ Module-11-React/fetchuserapp/src/Getuser.js
@@ -0,0 +1,79 @@
+﻿import React, { Component } from 'react';
+
+class Getuser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      firstname: '',
+      image: '',
+      loading: true,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.randomuser.me/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const user = data.results[0];
+        this.setState({
+          title: user.name.title,
+          firstname: user.name.first,
+          image: user.picture.large,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error: error.message, loading: false });
+      });
+  }
+
+  render() {
+    const { title, firstname, image, loading, error } = this.state;
+
+    if (loading) {
+      return <p>Loading user data...</p>;
+    }
+
+    if (error) {
+      return <p style={{ color: 'red' }}>Error: {error}</p>;
+    }
+
+    return (
+      <div style={styles.card}>
+        <img src={image} alt={`${title} ${firstname}`} style={styles.image} />
+        <h2 style={styles.name}>{title} {firstname}</h2>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  card: {
+    maxWidth: '300px',
+    margin: '20px auto',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '8px',
+    textAlign: 'center',
+    backgroundColor: '#fff'
+  },
+  image: {
+    width: '150px',
+    height: '150px',
+    borderRadius: '50%',
+    objectFit: 'cover'
+  },
+  name: {
+    marginTop: '15px',
+    fontSize: '20px'
+  }
+};
+
+export default Getuser;
