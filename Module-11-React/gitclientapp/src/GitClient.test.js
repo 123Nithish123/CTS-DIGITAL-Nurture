a847fd7268f08b98bd351b38189bb020ae400e7a
123Nithish123 <nithishkumarvasu2@gmail.com>
2026-07-14 11:10:46 +0530

    Add Module-11: Git Client App with GitHub API

---
+++ Module-11-React/gitclientapp/src/GitClient.test.js
@@ -0,0 +1,23 @@
+﻿import axios from 'axios';
+import GitClient from './GitClient';
+
+jest.mock('axios');
+
+describe('Git Client Tests', () => {
+
+  test('should return repository names for techiesyed', async () => {
+    const mockRepos = [
+      { name: 'react-project' },
+      { name: 'node-app' },
+      { name: 'python-scripts' }
+    ];
+
+    axios.get.mockResolvedValue({ data: mockRepos });
+
+    const repos = await GitClient.getRepositories('techiesyed');
+
+    expect(axios.get).toHaveBeenCalledWith('https://api.github.com/users/techiesyed/repos');
+    expect(repos).toEqual(['react-project', 'node-app', 'python-scripts']);
+  });
+
+});
