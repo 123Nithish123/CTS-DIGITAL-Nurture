424427c67dca99e995d3ceea89e398a75f4c9173
123Nithish123 <nithishkumarvasu2@gmail.com>
2026-07-15 14:15:47 +0530

    Add Module-11: Office Space Rental Application

---
+++ Module-11-React/officespacerentalapp/src/App.test.js
@@ -0,0 +1,14 @@
+﻿import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders office space rental heading', () => {
+  render(<App />);
+  const heading = screen.getByText(/office space rental app/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders office list items', () => {
+  render(<App />);
+  const officeNames = screen.getAllByText(/Name:/);
+  expect(officeNames.length).toBeGreaterThan(1);
+});
