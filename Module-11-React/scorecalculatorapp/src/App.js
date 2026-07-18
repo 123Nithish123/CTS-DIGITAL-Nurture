c6492670763da00ef0265e68250912f397e1f8f5
123Nithish123 <nithishkumarvasu2@gmail.com>
2026-07-18 10:30:21 +0530

    Add Module-11: Score Calculator with Custom Styles

---
+++ Module-11-React/scorecalculatorapp/src/App.js
@@ -0,0 +1,21 @@
+﻿import './Stylesheets/mystyle.css';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Student Management Portal</h1>
+        <p>Score Calculator</p>
+      </header>
+      <CalculateScore 
+        Name="John Doe" 
+        School="Springfield High" 
+        Total={450} 
+        goal={500} 
+      />
+    </div>
+  );
+}
+
+export default App;
